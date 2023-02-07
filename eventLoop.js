024207b602674da45f9d2249d9d22b50664c7eb4
Sweta <kumarisweta.as@gmail.com>
2023-02-07 19:07:54 +0530

    Update eventLoop.js

--- eventLoop.js
+++ eventLoop.js
@@ -2,6 +2,7 @@
 //classic way of how JS executes
 //i.e., how the queue is being emptied out
 //i.e., synchronous
+//https://www.google.com/amp/s/www.freecodecamp.org/news/synchronous-vs-asynchronous-in-javascript/amp/
 
 const uno = () => {
     console.log("I am One");
